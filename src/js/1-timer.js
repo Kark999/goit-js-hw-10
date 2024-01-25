2e2a272d6cc329b2643f19aab77690a69cf5659b
Andrey Svyrskyi <akkad1v@gmail.com>
2024-01-25 06:47:25 +0200

    'update'

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,12 +1,90 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/izitoast.min.css';
 
-const options = {
+const startButton = document.querySelector('.start');
+const timerContainer = document.querySelector('.timer');
+
+let userSelectedDate;
+
+flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+  onClose: function (selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate < new Date()) {
+      startButton.disabled = true;
+      iziToast.show({
+        title: 'Error',
+        message: 'Illegal operation?',
+      });
+    } else {
+      startButton.disabled = false;
+      userSelectedDate = selectedDate;
+    }
   },
-};
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function updateTimerDisplay(time) {
+  const daysElement = document.querySelector('[data-days]');
+  const hoursElement = document.querySelector('[data-hours]');
+  const minutesElement = document.querySelector('[data-minutes]');
+  const secondsElement = document.querySelector('[data-seconds]');
+
+  daysElement.textContent = addLeadingZero(time.days);
+  hoursElement.textContent = addLeadingZero(time.hours);
+  minutesElement.textContent = addLeadingZero(time.minutes);
+  secondsElement.textContent = addLeadingZero(time.seconds);
+}
+
+function startTimer() {
+  startButton.disabled = true;
+
+  const timerInterval = setInterval(function () {
+    const currentTime = new Date();
+    const timeDifference = userSelectedDate - currentTime;
+
+    if (timeDifference <= 0) {
+      clearInterval(timerInterval);
+      updateTimerDisplay({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      iziToast.success({
+        title: 'Countdown Complete',
+        message: 'The countdown timer has reached zero.',
+      });
+    } else {
+      const timeRemaining = convertMs(timeDifference);
+      updateTimerDisplay(timeRemaining);
+    }
+  }, 1000);
+}
+
+startButton.addEventListener('click', function () {
+  startTimer();
+});
